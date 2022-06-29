aa066e18c76ed9d00a70c6a35376370d0621d1c9
alicanerr <100146598+alicanerr@users.noreply.github.com>
2022-06-29 17:03:50 +0300

    Service and Mapping

---
+++ Galeri/Galeri.Service/Mappings/MapProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Galeri.Core.DTOs;
+using Galeri.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galeri.Service.Mappings
+{
+    public class MapProfiles:Profile
+    {
+        public MapProfiles()
+        {
+            CreateMap<Kategori,KategoriDto>().ReverseMap();
+            CreateMap<Araçlar,AraçlarDto>().ReverseMap();
+        }
+   
+    }
+}
---
+++ Galeri/Galeri.Service/Services/GenericServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galeri.Service.Services
+{
+    internal class GenericServices
+    {
+    }
+}
